fc483094aae096f8e8ae9e55d3dd0d083796130e
jae <wogud1514@naver.com>
2021-12-25 15:54:24 +0900

    프로퍼티 설정까지

---
+++ src/main/java/jae/jjfactory/login/TestDataInit.java
@@ -0,0 +1,21 @@
+package jae.jjfactory.login;
+
+import jae.jjfactory.login.domain.item.Item;
+import jae.jjfactory.login.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}

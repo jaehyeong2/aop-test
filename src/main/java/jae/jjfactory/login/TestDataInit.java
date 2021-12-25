53826c93482c9f7bcb38b5a5a024b5740d35831a
jae <wogud1514@naver.com>
2021-12-25 22:39:16 +0900

    회원가입까지

--- src/main/java/jae/jjfactory/login/TestDataInit.java
+++ src/main/java/jae/jjfactory/login/TestDataInit.java
@@ -2,6 +2,8 @@ package jae.jjfactory.login;
 
 import jae.jjfactory.login.domain.item.Item;
 import jae.jjfactory.login.domain.item.ItemRepository;
+import jae.jjfactory.login.domain.member.Member;
+import jae.jjfactory.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,10 +14,18 @@ import javax.annotation.PostConstruct;
 public class TestDataInit {
 
     private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
 
     @PostConstruct
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("1234!");
+        member.setName("테스터");
+
+        memberRepository.save(member);
     }
 }

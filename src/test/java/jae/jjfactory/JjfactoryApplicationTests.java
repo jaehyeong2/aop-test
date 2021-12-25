1504856c6b6020bca76e13dcc0a091a2dea32068
jae <wogud1514@naver.com>
2021-12-25 14:25:57 +0900

    세팅

---
+++ src/test/java/jae/jjfactory/JjfactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package jae.jjfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjfactoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

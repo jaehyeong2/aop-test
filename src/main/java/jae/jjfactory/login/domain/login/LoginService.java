1e2de463f6de31f880ae3bdcf9e5c527213452e9
jae <wogud1514@naver.com>
2021-12-25 23:38:43 +0900

    쿠키 로그인/만료

---
+++ src/main/java/jae/jjfactory/login/domain/login/LoginService.java
@@ -0,0 +1,23 @@
+package jae.jjfactory.login.domain.login;
+
+import jae.jjfactory.login.domain.member.Member;
+import jae.jjfactory.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
+
+
+}
--- src/main/java/jae/jjfactory/login/web/HomeController.java
+++ src/main/java/jae/jjfactory/login/web/HomeController.java
@@ -1,15 +1,55 @@
 package jae.jjfactory.login.web;
 
+import jae.jjfactory.login.domain.member.Member;
+import jae.jjfactory.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+@RequiredArgsConstructor
 @Controller
 @Slf4j
 public class HomeController {
 
+    private final MemberRepository memberRepository;
+
+//    @GetMapping("/")
+//    public String home(){
+//        return "home";
+//    }
+
     @GetMapping("/")
-    public String home(){
-        return "home";
+    public String homeLogin(@CookieValue(name = "memberId",required = false) Long memberId, Model model){
+
+        if(memberId == null){
+            return "home";
+        }
+
+        //로그인
+        Member loginMember = memberRepository.findById(memberId);
+        if(loginMember == null){
+            return "home";
+        }
+        model.addAttribute("member",loginMember);
+        return "loginHome";
+    }
+
+    @PostMapping("/logout")
+    public String logout(HttpServletResponse response){
+        expireCookie(response,"memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0);
+        response.addCookie(cookie);
     }
 }
